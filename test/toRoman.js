cb1b83d30229be07b91c255871446f484961fe2e
Arnaud ROUYER <josh.guthrie@gmail.com>
2013-07-22 21:48:20 +0200

    Version 0.2.1: Updating unit tests for toRoman

--- test/toRoman.js
+++ test/toRoman.js
@@ -10,65 +10,55 @@ var answers = [ 'I', 'IV', 'VIII', 'XV', 'XVI', 'XXIII', 'XLII', 'LXXXIV', 'C',
 
 
 var bad_literals = [ {}, [], 'foo', function () {} ];
-var bad_objects = [ new Object, new Array, new String('foo'), new Function() ];
+var bad_objects = [ new Object(), new Array(), new String('foo'), new Function() ];
 var neg_literals = literals.map(function (x) { return x * -1; });
 
-function makeThrow (x) {
-  assert.throws(function () { toRoman(x)});
+function wontThrow (arr) {
+  return function () {
+    arr.forEach(function (x) {
+      assert.doesNotThrow(function () { toRoman(x); });
+    });
+  };
 }
 
-function checkResults (x, i) {
-  assert.strictEqual(toRoman(x), answers[i]);
+function makeThrow (arr) {
+  return function () {
+    arr.forEach(function (x) {
+      assert.throws(function () { toRoman(x); });
+    });
+  };
+}
+
+function checkResults (arr) {
+  return function () {
+    arr.forEach(function (x, i) {
+      assert.strictEqual(toRoman(x), answers[i]);
+    });
+  };
 }
 
 describe('toRoman function:', function () {
   describe('Checking good inputs', function () {
-    it('Accepts literals numbers', function () {
-      literals.forEach(toRoman);
-    });
-    it('Accepts objects numbers', function () {
-      objects.forEach(toRoman);
-    });
-    it('Accepts string literal numbers', function () {
-      str_lit.forEach(toRoman);
-    });
-    it('Accepts string object numbers', function () {
-      str_obj.forEach(toRoman);
-    });
+    it('Accepts literals numbers', wontThrow(literals));
+    it('Accepts objects numbers', wontThrow(objects));
+    it('Accepts string literal numbers', wontThrow(str_lit));
+    it('Accepts string object numbers', wontThrow(str_obj));
   });
   describe('Checking wrong inputs', function () {
-    it('Throws on non-numbers literals', function () {
-      bad_literals.forEach(makeThrow);
-    });
-    it('Throws on non-number objects', function () {
-      bad_objects.forEach(makeThrow);
-    });
-    it('Throws on NaN', function () {
-      makeThrow(NaN);
-    });
-    it('Throws on negative numbers', function () {
-      neg_literals.forEach(makeThrow);
-    });
-    it('Throws on numbers over 3999', function () {
-      [ 4000, 4242 ].forEach(makeThrow)
-    });
+    it('Throws on non-numbers literals', makeThrow(bad_literals));
+    it('Throws on non-number objects', makeThrow(bad_objects));
+    it('Throws on NaN', makeThrow([ NaN ]));
+    it('Throws on negative numbers', makeThrow(neg_literals));
+    it('Throws on numbers over 3999', makeThrow([ 4000, 4242 ]));
   });
   describe('Checking results', function () {
     it('Works with a 0', function () {
       assert.strictEqual(toRoman(0), 'nulla');
       assert.strictEqual(toRoman(new Number(0)), 'nulla');
     })
-    it('Works with literals', function () {
-      literals.forEach(checkResults);
-    });
-    it('Works with objects', function () {
-      objects.forEach(checkResults);
-    });
-    it('Works with string literal numbers', function () {
-      str_lit.forEach(checkResults);
-    });
-    it('Works with string object numbers', function () {
-      str_obj.forEach(checkResults);
-    });
+    it('Works with literals', checkResults(literals));
+    it('Works with objects', checkResults(objects));
+    it('Works with string literal numbers', checkResults(str_lit));
+    it('Works with string object numbers', checkResults(str_obj));
   });
 });
